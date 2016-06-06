c65c23276254330c744bfa292cb7d45ce376af02
abinaya3 <abinayarakshana@gmail.com>
2016-06-06 11:22:19 +0530

    Create t.c

---
+++ t.c
@@ -0,0 +1,14 @@
+int main(){
+     int x=25,y=1;
+     do
+       if(x>5)
+         printf(" ONE");
+       else if(x>10)
+         printf(" TWO");
+       else if(x==25)
+         printf(" THREE");
+       else
+         printf(" FOUR");
+       while(y--);
+return 0;
+}
